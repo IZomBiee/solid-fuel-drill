6f2cae12b7d049e32354e3bb7793a774451cfeca
IZomBie <patrik009009@gmail.com>
2025-03-16 13:08:25 +0200

    Make first item compostable

--- src/main/java/izombiee/solidfueldrill/SolidFuelDrill.java
+++ src/main/java/izombiee/solidfueldrill/SolidFuelDrill.java
@@ -2,6 +2,10 @@ package izombiee.solidfueldrill;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +16,19 @@ public class SolidFuelDrill implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Item SUSPICIOUS_SUBSTANCE = ModItems.register("suspicious_substance", Item::new, new Item.Settings());
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		// Get the event for modifying entries in the ingredients group.
+		// And register an event handler that adds our suspicious item to the ingredients group.
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+				.register((itemGroup) -> itemGroup.add(SUSPICIOUS_SUBSTANCE));
+
+		CompostingChanceRegistry.INSTANCE.add(SUSPICIOUS_SUBSTANCE, 0.3f);
 
 		LOGGER.info("Hello Fabric world!");
 	}
